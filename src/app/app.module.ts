e5508fae3d3a3602ef1f3999292f2af2af63e4a2
dev <benjapaz57@gmail.com>
2023-09-27 11:50:18 -0300

    portfolio asd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
+import { FormComponent } from './components/form/form.component';
+import { CardsComponent } from './components/cards/cards.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    CarouselComponent,
+    FormComponent,
+    CardsComponent
   ],
   imports: [
     BrowserModule,
